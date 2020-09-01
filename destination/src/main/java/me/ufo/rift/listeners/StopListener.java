bb2817528cbb2b0410765a142bf00fefda1ff65b
uf0h <3z7qwnsqth@gmail.com>
2020-09-01 21:08:52 +0100

    more stopped event listeners

--- destination/src/main/java/me/ufo/rift/listeners/StopListener.java
+++ destination/src/main/java/me/ufo/rift/listeners/StopListener.java
@@ -3,12 +3,15 @@ package me.ufo.rift.listeners;
 import me.ufo.rift.Rift;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public final class StopListener implements Listener {
 
@@ -18,7 +21,7 @@ public final class StopListener implements Listener {
     this.plugin = plugin;
   }
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
   public void onPlayerJoin(final AsyncPlayerPreLoginEvent event) {
     if (this.plugin.isStopping()) {
       event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
@@ -26,29 +29,43 @@ public final class StopListener implements Listener {
     }
   }
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
   public void onBlockPlace(final BlockPlaceEvent event) {
     if (this.plugin.isStopping()) {
       event.setCancelled(true);
     }
   }
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
   public void onBlockBreak(final BlockBreakEvent event) {
     if (this.plugin.isStopping()) {
       event.setCancelled(true);
     }
   }
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
   public void onAsyncPlayerChat(final AsyncPlayerChatEvent event) {
     if (this.plugin.isStopping()) {
       event.setCancelled(true);
     }
   }
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
   public void onPlayerCommandPreprocess(final PlayerCommandPreprocessEvent event) {
+    if (this.plugin.isStopping()) {
+      event.setCancelled(true);
+    }
+  }
+
+  @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+  public void onPlayerMove(final PlayerMoveEvent event) {
+    if (this.plugin.isStopping()) {
+      event.setCancelled(true);
+    }
+  }
+
+  @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+  public void onPlayerTeleport(final PlayerTeleportEvent event) {
     if (this.plugin.isStopping()) {
       event.setCancelled(true);
     }
